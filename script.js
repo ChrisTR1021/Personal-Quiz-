6adafa35183936f51319bf8545abce85f6692991
ChrisTR1021 <christianridgell1021@gmail.com>
2023-11-01 03:38:55 -0400

    functions addedand letters removed

--- script.js
+++ script.js
@@ -4,67 +4,72 @@ var timeLeftEl = document.getElementById("timeRemaining")
 var nextButtonEl = document.getElementById("next-button")
 var containerEl= document.getElementById('quizContainer')
 var questionEl = document.getElementById('question')
-var i = 0
+var qindex = 0
 
 
 var myQuestions = [
     {
     id: '0',
     question: "What is JavaScript used for?",
-    answers: {
-        a: 'Web Applications',
-        b: 'Mobile Apps',
-        c: 'Game Development',
-        d: 'All of the above',
+    answers: [
+         'Web Applications',
+         'Mobile Apps',
+         'Game Development',
+         'All of the above',
 
-    },
-    correctAnswer: 'b'
+    ],
+    correctAnswer: 'All of the above'
 },
 {
     id: '1',
     question: 'Which of the following keywords is used to define a variable in JavaScript?',
-    answers: {
-        a:	'Var',
-        b:	'Let',
-        c:	'Both A and B',
-        d:	'None of the above',
+    answers: [
+        	'Var',
+        	'Let',
+        	'Both A and B',
+        	'None of the above',
 
-    },
-    correctAnswer: 'b'
+    ],
+    correctAnswer: 'Let'
 },
 {
     id: '2',
     question: 'Which built in method combines the text of two strings and returns a new string?',
-    answers: {
-        a:	'Append',
-        b:	'Concat',
-        c:	'Attach',
-        d:	'None of the above',
+    answers: [
+        	'Append',
+        	'Concat',
+        	'Attach',
+        	'None of the above',
 
-    },
-    correctAnswer: 'b'
+    ],
+    correctAnswer: 'Concat'
 },
 {
     id: '3',
     question:'Inside which HTML element do we put JavaScript?',
-    answers: {
-        a:	'<script>',
-        b:	'<javascript>',
-        c:	'<scripting>',
-        d:	'<js>',
+    answers: [
+        	'<script>',
+        	'<javascript>',
+        	'<scripting>',
+        	'<js>',
 
-    },
-    correctAnswer: 'a'
+],
+    correctAnswer: '<script>'
 }
 ];
 
-
 function nextQuestion() {
-    i++;
-    containerEl.innerHTML = '';
-    gameStart();
+    console.log(myQuestions[qindex].question)
+    qindex = qindex + 1
+    for (let i = 0; i <myQuestions[qindex].answers.length; i++) {
+        console.log(myQuestions[qindex].answers[i])
+        
+    }
+    // i++;
+    // containerEl.innerHTML = '';
+    // gameStart();
 }
 
 
-startButtonEl.addEventListener('click', gameStart);
+// startButtonEl.addEventListener('click', gameStart);
 nextButtonEl.addEventListener('click', nextQuestion);
